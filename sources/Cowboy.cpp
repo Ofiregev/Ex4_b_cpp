8c7a811642898ecee5b68692e4d355487d2d96d8
ofir <ofirmeidan1998@gmail.com>
2023-05-22 22:20:02 +0300

    editing

--- sources/Cowboy.cpp
+++ sources/Cowboy.cpp
@@ -2,67 +2,68 @@
 
 namespace ariel {
 
+    // Constructor for the Cowboy class.
     Cowboy::Cowboy(string name, Point location) : Character(location, 110, name), num_of_bullets(6) {}
 
+    // Setter for the number of bullets.
     void Cowboy::setBullets(int bul) {
-        if(!isAlive())
-        {
+        if (!isAlive()) {
             throw("the cowboy is dead");
         }
         num_of_bullets = bul;
     }
+
+    // Getter for the number of bullets.
     int Cowboy::getNumOfBullets() {
-        if(!isAlive())
-        {
+        if (!isAlive()) {
             throw("the cowboy is dead");
         }
         return num_of_bullets;
     }
    
 
-    void Cowboy::shoot(Character * enemy) {
-        if(this == enemy)
-        {
+    // Method to shoot at an enemy.
+    void Cowboy::shoot(Character *enemy) {
+        if (this == enemy) {
             throw std::runtime_error("cannot kill himself");
         }
-        if (hasboolets() && isAlive()&&enemy->isAlive()) {
+        if (hasboolets() && isAlive() && enemy->isAlive()) {
             double dist = distance(enemy);
             enemy->hit(10);
             num_of_bullets--;
-        }else if(!isAlive() ||!enemy->isAlive())
-        {
+        } else if (!isAlive() || !enemy->isAlive()) {
             throw std::runtime_error("the cowboy is dead");
-    }
+        }
     }
 
+    // Check if the cowboy has bullets.
     bool Cowboy::hasboolets() {
-        if(!isAlive())
-        {
+        if (!isAlive()) {
             throw std::runtime_error("the cowboy is dead");
         }
         return num_of_bullets > 0;
     }
 
+    // Method to reload the bullets.
     void Cowboy::reload() {
-        if(!isAlive())
-        {
+        if (!isAlive()) {
             throw std::runtime_error("the cowboy is dead");
         }
         num_of_bullets = 6;
     }
 
+    // Method to print the Cowboy's information.
     string Cowboy::print() {
-        if(!isAlive())
-        {
-            return "C: (" + getName() + ")" ;
-        }else{
-
-        return "C: " + getName() + ", Location: " + getLocation().print()+ ", Score: " + std::to_string(getScore()) + ", Bullets: " + std::to_string(num_of_bullets) ;
+        if (!isAlive()) {
+            return "C: (" + getName() + ")";
+        } else {
+            return "C: " + getName() + ", Location: " + getLocation().print() + ", Score: " + std::to_string(getScore()) + ", Bullets: " + std::to_string(num_of_bullets);
         }
     }
+
+    // Get the type information of the Cowboy class.
     const std::type_info& Cowboy::getType() const {
         return typeid(Cowboy);
     }
-    
 
-} // namespace ariel
+} 
--- sources/Cowboy.hpp
+++ sources/Cowboy.hpp
@@ -1,11 +1,11 @@
 #ifndef COWBOY_HPP
 #define COWBOY_HPP
+
 #include "Character.hpp"
 #include <iostream>
 #include <string>
 #include "Point.hpp"
-using namespace std;
-     
+
 namespace ariel {
     class Cowboy : public Character {
         private:
@@ -13,9 +13,12 @@ namespace ariel {
             
         public:
             Cowboy(string name, Point location);
+
+            // Override function to create a copy of the Cowboy object.
             Character* createCopy() const override {
-            return new Cowboy(*this);
-        }
+                return new Cowboy(*this);
+            }
+
             int getNumOfBullets();
             void setBullets(int bul);   
             void shoot(Character * enemy);
@@ -23,11 +26,7 @@ namespace ariel {
             void reload();
             string print() override;
             const std::type_info& getType() const override;
-
     };
 }
-
-    
-
 
 #endif
