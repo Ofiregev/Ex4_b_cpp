9f895c66216ad0dbe94967a83750545615019816
ofir <ofirmeidan1998@gmail.com>
2023-05-22 22:20:33 +0300

    editing

--- sources/YoungNinja.hpp
+++ sources/YoungNinja.hpp
@@ -3,16 +3,12 @@
 #include <iostream>
 #include <string>
 #include "Ninja.hpp"
-namespace ariel {
-    class YoungNinja : public Ninja {
-        public:
-            YoungNinja(string name, Point location);
-            // YountNinja(const YountNinja &other);
-            // YountNinja &operator=(const YountNinja &other);
-            // YountNinja(YountNinja &&other) noexcept;
-            // YountNinja &operator=(YountNinja &&other) noexcept;
-            // ~YountNinja() override; // add virtual keyword and define the destructor later
-
+namespace ariel
+{
+    class YoungNinja : public Ninja
+    {
+    public:
+        YoungNinja(string name, Point location);
     };
 }
 
